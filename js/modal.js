45723e53a62fba01dbedf9e3f7c03e5a2e2932a0
송병훈 <thdqudgns9612@gmail.com>
2023-03-17 14:51:31 +0900

    create modal

---
+++ js/modal.js
@@ -0,0 +1,13 @@
+
+function openModal() {
+	document.getElementById('modal').style.display='block';
+}
+
+function closeModal() {
+	document.getElementById('modal').style.display='none';
+}
+
+function checkId() { 
+    alert("사용 가능한 아이디입니다.");
+}
+
