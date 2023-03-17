488e2ae8d73e7621f2bc3cec8b4e4a73323a0b17
hyungJun <dofun8@gmail.com>
2023-03-17 16:04:49 +0900

    implement moral ( login, makeId )

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,12 @@
 
-function openModal() {
-	document.getElementById('modal').style.display='block';
+function openModal(data) {
+	document.getElementById('modal1').style.display = 'block';
+	location.href = data;
 }
 
 function closeModal() {
-	document.getElementById('modal').style.display='none';
+	document.getElementById('modal').style.display = 'none';
+	location.href = "main.html"
 }
 
 function checkId() { 
